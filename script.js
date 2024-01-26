99de38a9b4e16dcb8270dc8c571193e7b1439227
Anshoo Patel <126155374+Anshoo-Patel@users.noreply.github.com>
2024-01-26 18:49:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-(function ($) {
-  $(document).ready(function () {
-    // Function to show/hide content based on hash
-    // Update this function
-    function showContent() {
+// Wait for the document to load before running the script 
+$(document).ready(function () {
+  // Function to show/hide content based on hash
+  function showContent() {
     var region = location.hash || '#home';
     $('.content-region').hide();
     $(region).show();
     $('.main-menu a').removeClass('active');
     $('.main-menu a[href="' + region + '"]').addClass('active');
-    }
+  }
 
-
-    // Show content on page load and hashchange
-    $(window).on('load hashchange', function () {
-      showContent();
-    });
-
-    // Click event for navigation links
-    $('#content a').on('click', function (e) {
-      e.preventDefault();
-      var target = $(this).attr('href');
-      location.hash = target;
-      showContent();
-    });
-
-    // Click event for Home button
-    $('.main-menu li:first-child a').on('click', function (e) {
-      e.preventDefault();
-      location.hash = '#home';
-      showContent();
-    });
+  // Show content on page load and hashchange
+  $(window).on('load hashchange', function () {
+    showContent();
   });
-})(jQuery);
+
+  // Click event for navigation links
+  $('.main-menu a').on('click', function (e) {
+    e.preventDefault();
+    var target = $(this).attr('href');
+    location.hash = target;
+    showContent();
+
+    // If the target is #home, navigate to index.html
+    if (target === '#home') {
+      location.href = 'index.html';
+    }
+  });
+});
